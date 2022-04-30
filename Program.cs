719eaacbbb86dc428ad0abede97f4610d0f2c23a
Onur MUTLU <103672350+Konnichiwa-World@users.noreply.github.com>
2022-04-30 12:42:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Sınıf2
+{public class Tasit
+	{public String yakit;// Taşıtın yakıt tipi
+		public int hiz; // Taşıtın Maximum hızı 
+		public String renk; // Taşıtın rengi
+		public String marka; // Taşıtın markası
+		// Taşıtın bilgilerini ekrana yazdıran metot
+		public void tasitInfo() {
+			String tasit = "Taşıtın markası: " + marka + " rengi: " + renk + " yakıt tipi: " + yakit + " maximum hızı: " + hiz;
+			System.Console.WriteLine (tasit);
+		}
+	}
+	class MainClass
+	{public static void Main (string[] args)
+		{// Taşıt tipinden otomobil nesnesini oluşturuyoruz
+			Tasit otomobil = new Tasit ();
+			// nesnemize özellik değerlerini giriyoruz
+			otomobil.hiz = 220;
+			otomobil.yakit = "LPG";
+			otomobil.renk = "Siyah";
+			otomobil.marka = "Renault";
+			// Bilgileri ekrana yazdıracak metodu çağırıyoruz
+			otomobil.tasitInfo();
+		}
+	}
+}
+
